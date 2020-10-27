fccba9b20427a365f20c052cf0da98748df124cb
Damon Schulz <damon.schulz@gmail.com>
2020-10-27 18:52:14 -0500

    edited resume doc, added extra lines to about me and header, added profile image, addded new background image, deleted manifest, added favorite projects to portfolio section, added navy description added education info

--- my-resume-page/src/Components/Header.js
+++ my-resume-page/src/Components/Header.js
@@ -7,6 +7,8 @@ class Header extends Component {
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
+      var descriptionTwo= this.props.data.descriptionTwo;
+      var descriptionThree= this.props.data.descriptionThree;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
@@ -35,7 +37,11 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>I'm a {city} based <span>{occupation}</span></h3>
+            <h3> {description}.</h3>
+            <h3> {descriptionTwo}.</h3>
+            <h3> {descriptionThree}.</h3>
+
             <hr />
             <ul className="social">
                {networks}
